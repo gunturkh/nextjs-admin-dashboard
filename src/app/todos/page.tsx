d56ac5cfd7d54a90deaf4e24ce65471603bf0976
Guntur KH <guntur.kh@gmail.com>
2024-04-25 14:29:49 +0700

    feat: create todos page & nav sidebar

---
+++ src/app/todos/page.tsx
@@ -0,0 +1,29 @@
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
+import TableOne from "@/components/Tables/TableOne";
+import TableThree from "@/components/Tables/TableThree";
+import TableTwo from "@/components/Tables/TableTwo";
+
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+
+export const metadata: Metadata = {
+  title: "Next.js Todos",
+  description:
+    "This is Next.js Todos page for Dashboard",
+};
+
+const TodosPage = () => {
+  return (
+    <DefaultLayout>
+      <Breadcrumb pageName="Todos" />
+
+      <div className="flex flex-col gap-10">
+        <TableOne />
+        <TableTwo />
+        <TableThree />
+      </div>
+    </DefaultLayout>
+  );
+};
+
+export default TodosPage;
